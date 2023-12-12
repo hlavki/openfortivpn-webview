9cdc8d68b095669a09d74192a66b1ff69f7af528
Michal Hlavac <miso@hlavki.eu>
2023-12-12 23:18:36 +0100

    fix

--- openfortivpn-webview-qt/webpage.cpp
+++ openfortivpn-webview-qt/webpage.cpp
@@ -3,6 +3,9 @@
 WebPage::WebPage(QWidget *parent) : QWebEnginePage(parent) {
 }
 
+WebPage::WebPage(QWebEngineProfile *profile, QWidget *parent) : QWebEnginePage(profile, parent) {
+}
+
 void WebPage::certificateError(const QWebEngineCertificateError &certificateError) {
     // TODO: conditional treatment
     // return error.isOverridable();
