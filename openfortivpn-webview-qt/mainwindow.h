da5261dd3280707f8dd63fe6f0bc2770355495dd
Michal Hlavac <miso@hlavki.eu>
2023-12-12 23:39:50 +0100

    fix

--- openfortivpn-webview-qt/mainwindow.h
+++ openfortivpn-webview-qt/mainwindow.h
@@ -22,7 +22,6 @@ private slots:
     void onCookieRemoved(const QNetworkCookie &cookie);
     void updateTitle(const QString &title);
     void handleUrlChange(const QUrl &url);
-    void onCertificateError(const QWebEngineCertificateError error);
 
 private:
     QWebEngineProfile *webEngineProfile;
